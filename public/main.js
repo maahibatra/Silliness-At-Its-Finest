e9a19a0cd1c463dad834ecc533cfac845b005c9e
main <main@l0ser.local>
2025-01-08 19:30:07 +0530

    added a chicken that moos

--- public/main.js
+++ public/main.js
@@ -1,6 +1,12 @@
 const uselessButton = document.querySelector("#uselessButton");
+const chickenContainer = document.querySelector("#chickenContainer");
 
 uselessButton.addEventListener("mouseover", function() {
     uselessButton.style.top = Math.random() * 80 + "%";
     uselessButton.style.left = Math.random() * 80 + "%";
+})
+
+chickenContainer.addEventListener("mouseover", function() {
+    const mooSound = new Audio("moo.mp3");
+    mooSound.play();
 })
